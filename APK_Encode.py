74ac7fa3a5594b1a2aac6e422179f289a9c6e482
Alexander Kellogg <kelloggalexander@ufl.edu>
2024-04-11 20:04:01 -0400

    latest version

--- APK_Encode.py
+++ APK_Encode.py
@@ -13,3 +13,19 @@ def encode(password):
             newpassword.append(password[i] + 3)
     "".join(newpassword)
     return newpassword
+
+
+if __name__ == "__main__":
+    while True:
+        print("Menu\n-------------")
+        print("1. Encode\n2. Decode\n3. Quit\n")
+        choice = int(input("Please enter an option: "))
+        if choice == 3:
+            break
+        elif choice == 1:
+            password = input("Please enter your password to encode: ")
+            password = encode(password)
+            print("Your password has been encoded and stored!\n")
+        elif choice == 2:
+            new_pass = MS_decode.decode(password)
+            print(f"The encoded password is {password}, and the original password is {new_pass}.")
